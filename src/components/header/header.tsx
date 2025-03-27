991ce5714e456b52d20f8eabbf452d46979c943a
sandeep26899 <sandeepsj26899@gmail.com>
2025-03-28 00:16:41 +0530

    feature: add btn to my app

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,33 +1,43 @@
-import { clsx } from '@/utils/utils'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import logo from "../../../public/logo/logo.png"
+import { clsx } from "@/utils/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import logo from "../../../public/logo/logo.png";
+import Button from "../button/button";
 
 const Header = () => {
-    const [scrolled, setScrolled] = useState(0);
+  const [scrolled, setScrolled] = useState(0);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrolled(window.scrollY);
-        }
-        handleScroll();
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        }
-    }, [])
-    return (
-        <header className={clsx("header", scrolled && "scrolled")}>
-            <Container>
-                <div className="header_in">
-                    <Link href="/" className='logo'><Image src={logo} alt="logo" /></Link>
-                </div>
-            </Container>
-        </header>
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY);
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    )
-}
+  return (
+    <header className={clsx("header", scrolled && "scrolled")}>
+      <Container>
+        <div className="header_in">
+          {/* Logo */}
+          <Link href="/" className="logo">
+            <Image src={logo} alt="logo" />
+          </Link>
 
-export default Header
+          <Button
+            onClick={() => window.open("https://app.kryzel.io/", "_blank")}
+          >
+            My App
+          </Button>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
